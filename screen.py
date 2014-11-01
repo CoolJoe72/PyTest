2dee8ef909e94e9bcfc099ed01dffc7b040ecb70
Corvus <CoolJoe72@gmail.com>
2014-11-01 12:52:23 -0600

    added RPi detection and options

--- screen.py
+++ screen.py
@@ -11,24 +11,33 @@ import sys, os
 #from time import sleep
 #import pygame
 from platform import system, machine
+
+rpi = False
+
 if (system() == 'Linux' and machine() == 'armv6l'):
 	import RPi.GPIO as GPIO
 	
+	rpi = True
 	change = 22
 	end = 18
 	blue = 23
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(blue,GPIO.OUT, initial=GPIO.LOW)
+	blue_on, blue_off = GPIO.output(blue,1),GPIO.output(blue,0)
 	GPIO.setup(change,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(end,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def main():
-	person = input('Enter your name: ')
-	print('Hello', person)
+	if rpi:
+		blue_on
+	person = raw_input('Enter your name: ')
+	print 'Hello', person
 
 
 def cleanup():
+	if rpi:
+		GPIO.cleanup()
 	pygame.quit()
 	sys.exit()
 
