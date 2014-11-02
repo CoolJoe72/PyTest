14f0d304383e8415a6b7cf816063ed0e62c91a07
Corvus <CoolJoe72@gmail.com>
2014-11-01 19:15:59 -0600

    updated process for turning pins on and off

--- leds.py
+++ leds.py
@@ -20,11 +20,6 @@ GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
 GPIO.setup(bgreen, GPIO.OUT)
 GPIO.setup(bwhite, GPIO.OUT)
-blue_on, blue_off = GPIO.output(blue,1),GPIO.output(blue,0)
-red_on, red_off = GPIO.output(red,1),GPIO.output(red,0)
-green_on, green_off = GPIO.output(green,1),GPIO.output(green,0)
-bgreen_on, bgreen_off = GPIO.output(bgreen,1),GPIO.output(bgreen,0)
-bwhite_on, bwhite_off = GPIO.output(bwhite,1),GPIO.output(bwhite,0)
 GPIO.setup(end, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def main():
@@ -32,10 +27,17 @@ def main():
 	while True:
 		if not (GPIO.input(end)):
 			cleanup()
-		blue_on
-		sleep(.25)
-		blue_off
-		sleep(.25)
+		ledon(led[i])
+		sleep(.5)
+		ledoff(led[i])
+		sleep(.5)
+		i += 1
+
+def ledon(pin):
+	GPIO.output(pin,1)
+
+def ledoff(pin):
+	GPIO.output(pin,0)
 
 def cleanup():
 	print("end pushed")
