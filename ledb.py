e7441c10d3f847c8cd33eb9da713723b3c7a7280
Corvus <CoolJoe72@gmail.com>
2014-11-02 23:19:38 -0700

    Updated ledb.py to improvements from leds.py

--- ledb.py
+++ ledb.py
@@ -10,18 +10,16 @@ red = 21
 green = 19
 bgreen = 15
 bwhite = 13
-led = (blue, red, green, bgreen, bwhite)
+leds = (blue, red, green, bgreen, bwhite)
 ledn = ("Blue","Red","Green","Bright Green","Bright White")
 change = 22
 end = 18
 i = 0
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(blue, GPIO.OUT)
-GPIO.setup(red, GPIO.OUT)
-GPIO.setup(green, GPIO.OUT)
-GPIO.setup(bgreen, GPIO.OUT)
-GPIO.setup(bwhite, GPIO.OUT)
+for pin in leds:
+	GPIO.setup(pin, GPIO.OUT)
+
 GPIO.setup(change, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(end, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
@@ -32,10 +30,16 @@ def main():
 			change_led()
 		if not (GPIO.input(end)):
 			cleanup()
-		GPIO.output(led[i], 1)
+		ledon(led[i])
 		sleep(.25)
-		GPIO.output(led[i], 0)
+		ledoff(led[i])
 		sleep(.25)
+
+def ledon(pin):
+	GPIO.output(pin, 1)
+
+def ledoff(pin):
+	GPIO.output(pin, 0)
 
 def change_led():
 	global i
@@ -49,7 +53,6 @@ def cleanup():
 	print("end pushed")
 	GPIO.cleanup()
 	sys.exit()
-
 
 if __name__ == "__main__":
    main()
